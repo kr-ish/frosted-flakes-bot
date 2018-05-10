d0f7625e4179960ad6e246e172dbf7b196afa1d0
Krish Ravindranath <krish.ravindranath@gmail.com>
2018-05-09 20:22:08 -0400

    add comments for default python or env

--- frosty.sh
+++ frosty.sh
@@ -4,4 +4,9 @@ echo  # blank line
 date  # print date
 DIR=$(dirname "${BASH_SOURCE[0]}")
 cd $DIR
+
+# If you're using a virtual / conda environment
 ~/anaconda2/envs/frosted/bin/python frosty.py
+
+# If you're not
+# python frosty.py
