29af6ba6f0a5cb72803d952a60d0bff0ae069709
Krish Ravindranath <krish.ravindranath@gmail.com>
2019-01-12 17:29:45 -0500

    zalgo time

--- frosty.py
+++ frosty.py
@@ -3,6 +3,7 @@ import os
 import pickle
 import inflect
 import sys
+from random import choice, random
 from credentials import (
     consumer_key,
     consumer_secret,
@@ -10,6 +11,8 @@ from credentials import (
     access_token_secret,
 )
 
+
+# SETUP
 # Twitter API authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -18,17 +21,36 @@ api = tweepy.API(auth)
 # Setup inflect engine
 p = inflect.engine()
 
-# Path to pickle file which will store the times tweeted to disk
-times_tweeted_path = './times_tweeted.p'
+# Probability that determines when the zalgo process starts
+zalgo_trigger_probability = 0.005
 
-# Load count stored to disk if it exists
+
+# LOAD COUNTS
+# Path to pickle file which will store the times tweeted to disk
+times_tweeted_path = './times_tweeted1.p'
+
+# Path to pickle file which will store the zalgo number.
+# I could have put this in the times tweeted pickle, but
+# I think this is cleaner / backwards compatible.
+zalgo_number_path = './zalgo_number1.p'
+
+# Load count stored from disk if it exists
 if os.path.isfile(times_tweeted_path):
     with open(times_tweeted_path, 'rb') as f:
         times_tweeted = pickle.load(f)
 else:
     times_tweeted = 0
 
-# Construct the pre-proclamation count phrase
+# Load zalgo number from disk if it exists
+if os.path.isfile(zalgo_number_path):
+    with open(zalgo_number_path, 'rb') as f:
+        zalgo_number = pickle.load(f)
+else:
+    zalgo_number = -1  # zalgo not triggered
+
+
+# CONSTRUCT BASE TWEET
+# Construct the pre-proclamation count phrase.
 if times_tweeted == 0:
     pre_proclamation = ''
 else:
@@ -44,16 +66,47 @@ else:
         numeral_adverb)
 
 proclamation = 'Frosted Flakes is the greatest cereal of all time'
+tweet = pre_proclamation + proclamation
 
-# Attempt to update status, if it fails prints the error and exits
+
+# ZALGO TIME
+# If zalgo hasn't been triggered, apply trigger with trigger probability.
+if zalgo_number == -1:
+    if random() <= zalgo_trigger_probability:
+        zalgo_number = 1  # start at zalgo number 1
+# Apply zalgo to tweet string.
+else:
+    # number of zalgo marks added to each character
+    num_marks = int(zalgo_number ** 2 / 2)  # grows exponentially
+    marks = list(map(chr, range(768, 879)))  # combining diacritical marks
+    words = tweet.split()
+
+    # Randomly add marks to alphanumeric characters in each word, and join.
+    # The probability that a char gets marks increases with the zalgo number.
+    tweet = ' '.join(''.join(
+        c + ''.join(choice(marks) for _ in range(num_marks)
+                    if (random() * 100) < zalgo_number) * c.isalnum()
+        for c in word)
+        for word in words)
+
+
+# TWEET
+# Attempt to update status- print error and exit on failure
 try:
-    api.update_status(pre_proclamation + proclamation)
+    api.update_status(tweet)
 except Exception as e:
     print('Twitter Status Update Error: {}'.format(e))
     sys.exit(1)
 
-# Update counter on disk if tweet was succesfully posted
+
+# UPDATE
+# Update counters on disk if the tweet was successfully posted
 times_tweeted += 1
-print('Successfully tweeted, tweet number {}'.format(times_tweeted))
+if zalgo_number != -1:
+    zalgo_number += 1
+print('Successfully tweeted, tweet number {}, zalgo number {}'.format(
+    times_tweeted, zalgo_number))
 with open(times_tweeted_path, 'wb') as f:
     pickle.dump(times_tweeted, f)
+with open(zalgo_number_path, 'wb') as f:
+    pickle.dump(zalgo_number, f)
